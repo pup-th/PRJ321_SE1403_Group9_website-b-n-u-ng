e10058efb53a7717d7809984abe972db0ff01741
Quithce14632 <quitran189@gmail.com>
2020-07-20 20:30:41 +0700

    new system userDAO

--- BanDoUong/src/java/DAO/UserDAO.java
+++ BanDoUong/src/java/DAO/UserDAO.java
@@ -5,11 +5,13 @@
  */
 package DAO;
 
+import Entities.Orders;
 import Entities.Users;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -66,18 +68,92 @@ public class UserDAO {
         pst.setString(5, u.getuAddress());
         pst.executeUpdate();
     }
-    public Users getUserByEmail(String email){
+
+    public Users getUserByEmail(String email) {
         try {
             String sql = "SELECT * FROM `users` WHERE `uMail` = ?";
             PreparedStatement pst = conn.prepareStatement(sql);
             pst.setString(1, email);
             ResultSet rs = pst.executeQuery();
-            if(rs.next()){
-                return new Users(rs.getString("uMail"), 
-                        rs.getString("uPassword"), 
+            if (rs.next()) {
+                return new Users(rs.getString("uMail"),
+                        rs.getString("uPassword"),
                         rs.getString("uName"),
-                        rs.getString("uPhone"), 
+                        rs.getString("uPhone"),
                         rs.getString("uAddress"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public Entities.Users getInfoUser(String mail) {
+        try {
+            PreparedStatement pst = conn.prepareStatement("SELECT * FROM `users` WHERE `uMail`=?");
+            pst.setString(1, mail);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                Entities.Users u = new Users(rs.getString("uMail"), rs.getString("uPassword"), rs.getString("uName"), rs.getString("uPhone"), rs.getString("uAddress"));
+                return u;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public int updateInfo(String cemail, String cname, String cphone, String caddress, String omail) {
+        try {
+            PreparedStatement pst = conn.prepareStatement("UPDATE `users` SET `uMail`=?,`uName`=?,`uPhone`=?,`uAddress`=? WHERE `uMail`=?");
+            pst.setString(1, cemail);
+            pst.setString(2, cname);
+            pst.setString(3, cphone);
+            pst.setString(4, caddress);
+            pst.setString(5, omail);
+            return pst.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+
+    public int updatePassword(String email, String newpass, String oldpass) {
+        try {
+            PreparedStatement pst = conn.prepareStatement("UPDATE `users` SET `uPassword`=MD5('?') WHERE `uMail`=? AND `uPassword`=MD5('?')");
+            pst.setString(1, newpass);
+            pst.setString(2, email);
+            pst.setString(3, oldpass);
+            return pst.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+
+    public ArrayList<Entities.Orders> getHisPurchase(String mail) {
+        try {
+            ArrayList<Entities.Orders> list = new ArrayList<>();
+            PreparedStatement pst = conn.prepareStatement("SELECT * FROM `orderdetail` WHERE `uMail`=?");
+            pst.setString(1, mail);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                list.add(new Orders(rs.getString("iName"), rs.getInt("quantity"), rs.getInt("priceEachitem"), rs.getDate("orderDate"), rs.getInt("total")));
+            }
+            return list;
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public String getPassword(String mail) {
+        try {
+            PreparedStatement pst = conn.prepareStatement("SELECT `uPassword` FROM `users` WHERE `uMail`=?");
+            pst.setString(1, mail);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                return rs.getString("uPassword");
             }
         } catch (SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
